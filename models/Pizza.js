1b27ce05fa7d23c2b07750917d16d1ea2e1f775b
Caleb E <mkcnj01@gmail.com>
2025-01-14 20:39:17 -0600

    Finished Create Pizza Setup

---
+++ models/Pizza.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+//create the Pizza Model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+//Export the Model
+module.exports = Pizza;
